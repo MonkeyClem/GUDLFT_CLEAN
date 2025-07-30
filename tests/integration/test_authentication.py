e557f9680a51d352bf0dc7eaf04a8b0f65b79f5a
Clément | Igonogo <clement@igonogo.io>
2025-07-30 11:25:40 +0200

    Added doctype tag in html files

--- tests/integration/test_authentication.py
+++ tests/integration/test_authentication.py
@@ -12,13 +12,6 @@ def test_invalid_email_redirect():
         assert response.status_code == 200
         assert b"ERROR : Unknown e-mail" in response.data
 
-
-# def test_logout_route_should_redirect_to_index():
-#     with app.test_client() as client:
-#         response = client.get('/logout', follow_redirects=True)
-#         assert response.status_code == 200
-#         assert b"Welcome" in response.data or b"Login" in response.data
-
 def test_logout_redirects_to_index():
     with app.test_client() as client:
         response = client.get('/logout', follow_redirects=True)
